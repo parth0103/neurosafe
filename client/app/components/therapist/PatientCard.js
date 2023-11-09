d69c2f4c8490c1fdaa566fad37c34a52eb2dfc41
goldenPheonix02 <jaipateluhm15@gmail.com>
2023-11-09 18:52:23 +0530

    scrollbal fixed

--- client/app/components/therapist/PatientCard.js
+++ client/app/components/therapist/PatientCard.js
@@ -1,23 +1,27 @@
 'use client';
-import { useState } from "react";
+import { useState } from 'react';
 
-export default function PatientCard(props){
-    const [patient, setPatient] = useState({})
-    console.log(patient)
-    return (
-		<div className="p-2 bg-white w-full flex flex-row items-center justify-between">
-			<div className="flex flex-row items-center space-x-3 text-sm">
-				<div className="">
-					<img src={props.profilePic} alt="" className="h-10" />
-				</div>
-				<div className="font-semibold">{props.name}</div>
-			</div>
-			<div className="text-xs">
-				<button  className="bg-neurosafe-primary p-2 rounded-sm text-white border-2 border-neurosafe-primary hover:bg-white hover:text-neurosafe-primary duration-150 transition-all"
-                onClick={() => {
-                    setPatient(props)
-                }}>View Details</button>
-			</div>
-		</div>
-	);
+export default function PatientCard(props) {
+  const [patient, setPatient] = useState({});
+  console.log(patient);
+  return (
+    <div className="p-2 bg-white w-full flex flex-row items-center justify-between mx-auto">
+      <div className="flex flex-row items-center space-x-3 text-sm">
+        <div className="">
+          <img src={props.profilePic} alt="" className="h-10" />
+        </div>
+        <div className="font-semibold">{props.name}</div>
+      </div>
+      <div className="text-xs">
+        <button
+          className="bg-neurosafe-primary p-2 rounded-sm text-white border-2 border-neurosafe-primary hover:bg-white hover:text-neurosafe-primary duration-150 transition-all"
+          onClick={() => {
+            setPatient(props);
+          }}
+        >
+          View Details
+        </button>
+      </div>
+    </div>
+  );
 }
