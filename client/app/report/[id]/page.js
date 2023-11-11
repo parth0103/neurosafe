d59318bbfebed7ef75d60bd9feec63d75cd73bb3
goldenPheonix02 <jaipateluhm15@gmail.com>
2023-11-11 22:52:17 +0530

    ipfs upload

--- client/app/report/[id]/page.js
+++ client/app/report/[id]/page.js
@@ -1,15 +1,19 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import EmotionBar from '@/app/components/ReportGraphs/EmotionBar';
 import MoodTrend from '@/app/components/ReportGraphs/MoodTrend';
 import Wrapper from '@/app/components/global/Wrapper';
 import html2canvas from 'html2canvas';
 import { Button } from 'react-bootstrap';
 import axios from 'axios';
+import { Spinner } from 'react-bootstrap';
 import { saveAs } from 'file-saver';
 
 import jsPDF from 'jspdf';
+import { toast } from 'react-toastify';
 export default function page() {
+  const [loading, setloading] = useState(false);
+  const [url, seturl] = useState(null);
   const uploadBlob = async (blob) => {
     var formData = new FormData();
     console.log(blob);
@@ -23,10 +27,21 @@ export default function page() {
         body: formData,
       },
       { headers: { 'Content-Type': 'multipart/form-data' } }
-    ).then((e) => console.log(e));
+    )
+      .then((res) => res.json())
+      .then((response) => {
+        seturl(response.url);
+        toast('Saved Successfully!', 'success');
+        console.log(response);
+      })
+      .catch((err) => console.log(err))
+      .finally(() => {
+        setloading(false);
+      });
   };
   const printDocument = async () => {
     const input = document.getElementById('reportSection');
+    setloading(true);
     html2canvas(input).then((canvas) => {
       const imgData = canvas.toDataURL('image/png');
       const pdf = new jsPDF({ format: [500, 450] });
@@ -50,9 +65,40 @@ export default function page() {
     <>
       <Wrapper>
         <div className="p-2">
+          <div className="flex justify-center gap-5 mb-5">
+            {loading ? (
+              <div className="flex gap-4 items-center">
+                <div>Saving</div> <Spinner variant="success" />
+              </div>
+            ) : (
+              <>
+                <Button
+                  className=""
+                  variant="outline-success"
+                  onClick={() => {}}
+                >
+                  Print To Pdf
+                </Button>
+                <Button variant="outline-success" onClick={printDocument}>
+                  Save Report
+                </Button>
+              </>
+            )}
+            {url == null ? (
+              ''
+            ) : (
+              <div>
+                <Button variant="outline-primary">
+                  <a href={url} target="_blank" className="color-black">
+                    View File
+                  </a>
+                </Button>
+              </div>
+            )}
+          </div>
           <div
             id="reportSection"
-            className="text-center w-[1400px] mx-auto pt-5"
+            className="text-center w-[1400px] mx-auto pt-5 border-1 pb-3 rounded-md border-gray"
           >
             <div className="flex flex-col justify-center items-center">
               <div className="text-2xl font-bold self-center mb-4">
@@ -85,11 +131,6 @@ export default function page() {
               </div>
             </div>
           </div>
-        </div>
-        <div className="flex justify-center">
-          <Button variant="outline-success" onClick={printDocument}>
-            Print To Pdf
-          </Button>
         </div>
       </Wrapper>
     </>
